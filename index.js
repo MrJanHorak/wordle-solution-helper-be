b485905281dd067c829df93c7424b2546108bbeb
MrJanHorak <janny_b_goode@hotmail.com>
2024-03-23 22:47:30 -0400

    refactor to function as a firebase function

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
+const functions = require('firebase-functions');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const url = 'https://www.rockpapershotgun.com/wordle-past-answers';
+exports.scrapeWords = functions.https.onRequest((request, response) => {
+  const url = 'https://www.rockpapershotgun.com/wordle-past-answers';
 
-axios.get(url)
-  .then(response => {
-    const html = response.data;
-    const $ = cheerio.load(html);
-    const wordList = [];
+  axios.get(url)
+    .then(res => {
+      const html = res.data;
+      const $ = cheerio.load(html);
+      const wordList = [];
 
-    // The selector will depend on the actual structure of the webpage
-    // This is a generic example and will likely need to be adjusted
-    $('ul.inline > li').each(function() {
-      const word = $(this).text();
-      wordList.push(word);
+      $('ul.inline > li').each(function() {
+        const word = $(this).text();
+        wordList.push(word);
+      });
+      response.send(wordList);
+    })
+    .catch(error => {
+      console.error(error);
+      response.status(500).send(error);
     });
-
-    console.log(wordList);
-  })
-  .catch(console.error);
+});
