4f68cdf142c1cfe2d08d4bd37a446d3d6487c6b9
MrJanHorak <janny_b_goode@hotmail.com>
2024-03-24 22:58:30 -0400

    refactor to gen1 firebase function

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
-const functions = require('firebase-functions');
-const axios = require('axios');
-const cheerio = require('cheerio');
+import functions from 'firebase-functions';
+import express from 'express';
+import cheerio from 'cheerio';
+import axios from 'axios';
+import cors from 'cors';
+import NodeCache from 'node-cache';
 
-exports.scrapeWords = functions.https.onRequest((request, response) => {
-  const url = 'https://www.rockpapershotgun.com/wordle-past-answers';
+const app = express();
+app.use(cors());
 
-  axios.get(url)
-    .then(res => {
-      const html = res.data;
-      const $ = cheerio.load(html);
-      const wordList = [];
+const cache = new NodeCache();
+const cacheKey = "wordle_list";
+const scrapeInterval = 24 * 60 * 60; // Cache duration (24 hours in seconds)
 
-      $('ul.inline > li').each(function() {
-        const word = $(this).text();
-        wordList.push(word);
-      });
-      response.send(wordList);
-    })
-    .catch(error => {
-      console.error(error);
-      response.status(500).send(error);
-    });
+app.get("/", (request, response) => {
+  res.send("Hello from Firebase!");
 });
+
+app.get('/scrapeWords', async (req, res) => {
+  // Check cache for fresh data
+  const cachedWords = cache.get(cacheKey);
+  if (cachedWords && Date.now() - cache.get(cacheKey).fetchedAt < scrapeInterval) {
+    console.log("Using cached word list");
+    return res.json(cachedWords.data);
+  }
+
+  try {
+    const url = 'https://www.rockpapershotgun.com/wordle-past-answers';
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const wordList = [];
+    $('ul.inline > li').each(function() {
+      const word = $(this).text();
+      wordList.push(word);
+    });
+
+    // Update cache with fresh data
+    cache.set(cacheKey, { data: wordList, fetchedAt: Date.now() });
+
+    res.json({wordList:1});
+  } catch (error) {
+    console.error("Error scraping words:", error);
+    res.status(500).send("Error fetching word list");
+  }
+});
+
+export const api = functions.https.onRequest(app);
